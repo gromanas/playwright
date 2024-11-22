005e1f68f6f9ec045a7e6c3e354e793baf517887
gromanas <giorgos.romanas@camelotls.com>
2024-11-22 18:34:33 +0200

    [step-1] - first test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,5 +5,4 @@ test('test', async ({ page }) => {
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.locator('[data-test="secondary-header"]')).toBeVisible();
 });
